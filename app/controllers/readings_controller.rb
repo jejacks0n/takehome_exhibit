a64a8f2d5271adf868a1ead6a8602664715dfc23
Jeremy Jackson <jejacks0n@gmail.com>
2026-01-09 16:04:20 -0700

    Define and test the behavior (not final, basic)

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,19 +1,53 @@
 class ReadingsController < ActionController::API
-  before_action :find_or_create_device
+  before_action :find_or_initialize_device_data, only: :create
+
+  rescue_from ActionController::ParameterMissing do
+    render json: { error: "Invalid parameters" }, status: :unprocessable_content
+  end
 
   def create
+    Array(permitted_params[:readings]).each do |reading|
+      timestamp = reading[:timestamp]
+      next if @device_data[:timestamps].key?(timestamp) # ignore duplicates
 
+      time = Time.parse(timestamp) rescue next # skip invalid timestamps
+      count = reading[:count].to_i # ensure count values are integers
 
-    render json: { success: true }, status: :created
+      @device_data[:timestamps][timestamp] = count
+      @device_data[:cumulative_count] += count
+
+      # Keep track of the latest timestamp.
+      if @device_data[:latest_timestamp].nil? || time > @device_data[:latest_timestamp]
+        @device_data[:latest_timestamp] = time
+      end
+
+      # We can write to the cache each time to try to reduce race conditions, but there
+      # are downsides if this wasn't just in memory. Syncing the data to a persistent
+      # datastore (even if just in memory) wouldn't be optimal as the data grows, so
+      # we're left with improving the architecture/removing the limitations, or having a higher likelihood of race conditions.
+      Rails.cache.write("device_#{permitted_params[:id]}", @device_data)
+    end
+
+    # We can also just write the cache data at the end of the loop, which is fine, but
+    # does incur a higher risk of race conditions in high throughput / high concurrency
+    # systems.
+    # Rails.cache.write("device_#{permitted_params[:id]}", @device_data)
+
+    render json: { message: "Readings created" }, status: :created
   end
 
   private
 
-  def find_or_create_device
-    @device = Device.find_or_create_by(id: permitted_params[:id])
+  def find_or_initialize_device_data
+    @device_data = Rails.cache.fetch("device_#{permitted_params[:id]}") do
+      { timestamps: {}, cumulative_count: 0, latest_timestamp: nil }
+    end
   end
 
   def permitted_params
+    # We don't explicitly need permitted params without ActiveRecord, but we can use them
+    # for some semblance of schema enforcement. There's better ways to do this with, like
+    # with ActiveRecord for example, but I don't think that's what the exercise is for.
     @permitted_params ||= begin
       params.require(:id)
       params.require(:readings)
---
+++ spec/controllers/readings_controller_spec.rb
@@ -0,0 +1,111 @@
+require "spec_helper"
+
+RSpec.describe ReadingsController, type: :controller do
+  let(:device_id) { "36d5658a-6908-479e-887e-a949ec199272" }
+  let(:cache_key) { "device_#{device_id}" }
+
+  before do
+    Rails.cache.clear
+  end
+
+  describe "POST #create" do
+    let(:valid_attributes) do
+      {
+        id: device_id,
+        readings: [
+          { timestamp: "2021-09-29T16:08:15+01:00", count: 5 },
+          { timestamp: "2021-09-29T16:09:15+01:00", count: 6 }
+        ]
+      }
+    end
+
+    context "with valid params" do
+      it "stores the readings in the cache" do
+        post :create, params: valid_attributes
+
+        expect(response).to have_http_status(:created)
+
+        expect(cached_data = Rails.cache.read(cache_key)).to be_present
+        expect(cached_data[:cumulative_count]).to eq(11)
+        expect(cached_data[:latest_timestamp]).to eq(Time.parse("2021-09-29T16:09:15+01:00"))
+        expect(cached_data[:timestamps].keys).to contain_exactly("2021-09-29T16:08:15+01:00", "2021-09-29T16:09:15+01:00")
+      end
+
+
+      it "ignores duplicate readings" do
+        post :create, params: valid_attributes
+        post :create, params: valid_attributes
+
+        cached_data = Rails.cache.read(cache_key)
+        expect(cached_data[:cumulative_count]).to eq(11) # should not double
+        expect(cached_data[:timestamps].length).to eq(2)
+      end
+
+      it "updates existing device data" do
+        post :create, params: valid_attributes.merge(readings: [{ timestamp: "2021-09-29T16:08:15+01:00", count: 5 }])
+
+        cached_data = Rails.cache.read(cache_key)
+        expect(cached_data[:cumulative_count]).to eq(5)
+        expect(cached_data[:latest_timestamp]).to eq(Time.parse("2021-09-29T16:08:15+01:00"))
+
+        post :create, params: valid_attributes.merge(readings: [{ timestamp: "2021-09-29T16:09:15+01:00", count: 6 }])
+
+        cached_data = Rails.cache.read(cache_key)
+        expect(cached_data[:cumulative_count]).to eq(11)
+        expect(cached_data[:latest_timestamp]).to eq(Time.parse("2021-09-29T16:09:15+01:00"))
+      end
+
+      it "handles out of order readings" do
+        valid_attributes[:readings].reverse!
+
+        post :create, params: valid_attributes
+
+        cached_data = Rails.cache.read(cache_key)
+        expect(cached_data[:cumulative_count]).to eq(11)
+        expect(cached_data[:latest_timestamp]).to eq(Time.parse("2021-09-29T16:09:15+01:00"))
+      end
+    end
+
+    context "with invalid params" do
+      it "responds with an error message (422)" do
+        post :create, params: { }
+
+        expect(response).to have_http_status(:unprocessable_content)
+
+        json_response = JSON.parse(response.body)
+        expect(json_response["error"]).to eq("Invalid parameters")
+      end
+    end
+
+    context "with bad data" do
+      it "doesn't store any readings if it's not an array" do
+        post :create, params: { id: device_id, readings: "_bad_readings_" }
+
+        cached_data = Rails.cache.read(cache_key)
+        expect(cached_data[:cumulative_count]).to eq(0)
+        expect(cached_data[:timestamps].length).to eq(0)
+      end
+
+      it "doesn't count readings with invalid timestamps" do
+        valid_attributes[:readings].unshift({ timestamp: "_invalid_timestamp_", count: 10 })
+
+        post :create, params: valid_attributes
+
+        cached_data = Rails.cache.read(cache_key)
+        expect(cached_data[:cumulative_count]).to eq(11)
+        expect(cached_data[:timestamps].length).to eq(2)
+      end
+
+      it "handles invalid count readings" do
+        valid_attributes[:readings].unshift({ timestamp: "2021-09-29T16:10:15+01:00", count: 'm' })
+
+        post :create, params: valid_attributes
+
+        cached_data = Rails.cache.read(cache_key)
+        expect(cached_data[:cumulative_count]).to eq(11)
+        expect(cached_data[:timestamps].length).to eq(3)
+        expect(cached_data[:timestamps]['2021-09-29T16:10:15+01:00']).to eq(0)
+      end
+    end
+  end
+end
