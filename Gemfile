cedbfde043df2ffb5aa2178ccbc420fe2d300e4c
Jeremy Jackson <jejacks0n@gmail.com>
2026-01-09 14:43:06 -0700

    Initial commit (project setup)

---
+++ Gemfile
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "rails", "~> 8.1"
+gem "puma", "~> 7.1"
+gem "standard", "~> 1.52"
+gem "rspec-rails", "~> 8.0"
---
+++ config/application.rb
@@ -0,0 +1,32 @@
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
+
+require "bundler/setup" # Set up gems listed in the Gemfile.
+
+require "rails"
+require "action_controller/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Takehome
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 8.1
+
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    # Eager load code on boot.
+    config.eager_load = true
+  end
+end
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+# Draw the routes for the application.
+Rails.application.routes.draw do
+end
